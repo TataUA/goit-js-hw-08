f8a19ac8d4f84cdf1aee60e34fd407e88b7f621b
TataUA <khyzhnjaktetjana@gmail.com>
2023-05-03 02:04:35 +0300

    exercise 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({seconds}) {
+    localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case !STORAGE_KEY:
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+
+const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const dataForm = {};
+
+feedbackForm.addEventListener('input', throttle(onInputForm, 500));
+feedbackForm.addEventListener('submit', onSubmitForm);
+forDataFill();
+
+
+function onInputForm(e) {
+    dataForm[e.target.name] = e.target.value;
+    
+    const dataJSON = JSON.stringify(dataForm);
+
+    localStorage.setItem(STORAGE_KEY, dataJSON);
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function forDataFill() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(savedData) {
+        email.value = savedData.email;
+        message.value = savedData.message;
+    }    
+}
